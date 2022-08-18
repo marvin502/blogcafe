b8c8398f48566daaef133f719a82366244316cb4
marvin502 <62572934+marvin502@users.noreply.github.com>
2022-08-18 12:25:53 -0600

    Add files via upload

---
+++ cursos.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlogDeCafé</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor">
+            <div class="barra">
+                <a class="logo" href="index.php">
+                   <h1 class="logo__nombre no-margin centrar-texto">Blog<span class="logo__bold">DeCafé</span></h1>
+                </a>
+                <nav class="navegacion">
+                    <a href="nosotros.php" class="navegacion__enlace">Nosotros</a>
+                    <a href="cursos.php" class="navegacion__enlace">Cursos</a>
+                    <a href="contacto.php" class="navegacion__enlace">contacto</a>
+                </nav>
+            </div>
+        </div>
+        <div class="header__texto">
+            <h2 class="no-margin">Blog de café con consejos y cursos</h2>
+            <p class="no-margin">Aprende de los expertos con las mejores recetas y consejos</p>
+        </div>
+    </header>
+
+    <main class="contenedor">
+        <h3 class="centrar-texto">Nuestros Próximos Cursos y Talleres</h3>
+        <div class="curso">
+            <div class="curso__imagen">
+                <img src="curso1.jpg" alt="Imagen del Curso">
+            </div>
+           <div class="curso__informacion">
+            <h4 class="no-margin">Técnicas de Extracción de Café</h4>
+            <p class="curso__label">Precio:
+                <span class="curso__info">Gratis</span>
+            </p>
+            <p class="curso__label">Cupo:
+                <span class="curso__info">20</span>
+            </p>
+            <p class="curso__descripcion">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eos iure architecto, at fugit beatae nam debitis dolore, possimus magnam eveniet sit suscipit eius cupiditate aliquid, ipsum nemo temporibus assumenda.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto velit rem iure tempore saepe reiciendis totam consectetur dolorum. Exercitationem omnis eum unde quo et nulla quae laboriosam, adipisci iure sed.
+            </p>
+           </div><!--Curso informacion-->
+        </div><!--Curso-->
+
+        <div class="curso">
+            <div class="curso__imagen">
+                <img src="curso2.jpg" alt="Imagen del Curso">
+            </div>
+           <div class="curso__informacion">
+            <h4 class="no-margin">Taller para Tostar y Moler Granos</h4>
+            <p class="curso__label">Precio:
+                <span class="curso__info">Gratis</span>
+            </p>
+            <p class="curso__label">Cupo:
+                <span class="curso__info">20</span>
+            </p>
+            <p class="curso__descripcion">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eos iure architecto, at fugit beatae nam debitis dolore, possimus magnam eveniet sit suscipit eius cupiditate aliquid, ipsum nemo temporibus assumenda.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto velit rem iure tempore saepe reiciendis totam consectetur dolorum. Exercitationem omnis eum unde quo et nulla quae laboriosam, adipisci iure sed.
+            </p>
+           </div><!--Curso informacion-->
+        </div><!--Curso-->
+
+        <div class="curso">
+            <div class="curso__imagen">
+                <img src="curso3.jpg" alt="Imagen del Curso">
+            </div>
+           <div class="curso__informacion">
+            <h4 class="no-margin">4 Recetas de Café para principiantes</h4>
+            <p class="curso__label">Precio:
+                <span class="curso__info">Gratis</span>
+            </p>
+            <p class="curso__label">Cupo:
+                <span class="curso__info">20</span>
+            </p>
+            <p class="curso__descripcion">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis eos iure architecto, at fugit beatae nam debitis dolore, possimus magnam eveniet sit suscipit eius cupiditate aliquid, ipsum nemo temporibus assumenda.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto velit rem iure tempore saepe reiciendis totam consectetur dolorum. Exercitationem omnis eum unde quo et nulla quae laboriosam, adipisci iure sed.
+            </p>
+           </div><!--Curso informacion-->
+        </div><!--Curso-->
+      
+    </main>
+
+    <footer class="footer">
+        <div class="contenedor">
+            <div class="barra">
+                <a class="logo" href="index.php">
+                   <h1 class="logo__nombre no-margin centrar-texto">Blog<span class="logo__bold">DeCafé</span></h1>
+                </a>
+                <nav class="navegacion">
+                    <a href="nosotros.php" class="navegacion__enlace">Nosotros</a>
+                    <a href="cursos.php" class="navegacion__enlace">Cursos</a>
+                    <a href="contacto.php" class="navegacion__enlace">contacto</a>
+                </nav>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ entrada.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlogDeCafé</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor">
+            <div class="barra">
+                <a class="logo" href="index.php">
+                   <h1 class="logo__nombre no-margin centrar-texto">Blog<span class="logo__bold">DeCafé</span></h1>
+                </a>
+                <nav class="navegacion">
+                    <a href="nosotros.php" class="navegacion__enlace">Nosotros</a>
+                    <a href="cursos.php" class="navegacion__enlace">Cursos</a>
+                    <a href="contacto.php" class="navegacion__enlace">contacto</a>
+                </nav>
+            </div>
+        </div>
+        <div class="header__texto">
+            <h2 class="no-margin">Blog de café con consejos y cursos</h2>
+            <p class="no-margin">Aprende de los expertos con las mejores recetas y consejos</p>
+        </div>
+    </header>
+
+    <main class="contenedor">
+        <h3 class="centrar-texto">Sobre Nosotros</h3>
+        <img src="blog1.jpg" alt="Imagen del blog">
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate finibus lacus, non aliquam nulla tincidunt et. Cras facilisis tincidunt enim. Sed ac diam in est dapibus pharetra sed sed ligula. Ut sed tempor lacus. Cras gravida auctor lectus sit amet semper. Donec justo metus, posuere vel semper id, eleifend accumsan dolor. Donec dapibus nunc quis ante interdum accumsan. Mauris sit amet tristique dui.
+        </p>
+
+        <p>
+        Nunc justo lectus, laoreet et scelerisque non, placerat at magna. Duis ut ligula in erat varius pretium et ut mi. Pellentesque sollicitudin sed nulla ut lacinia. Nullam quis risus vel mi lobortis consequat. Vivamus venenatis lectus mi. Proin sed efficitur mi. Maecenas fringilla viverra ex, in tincidunt turpis gravida in. Sed sed turpis enim. Phasellus ac risus sit amet felis ullamcorper eleifend eget aliquam quam. Curabitur eu eros rhoncus nisi condimentum ullamcorper.
+        </p>
+
+        <p>
+        Aliquam a purus mollis mi pretium molestie non quis ex. Nunc laoreet ex sed neque pulvinar, a ultrices erat gravida. Donec vel nulla malesuada, varius eros eu, vulputate velit. Vivamus varius, neque non tristique tristique, magna augue porttitor nibh, nec iaculis urna mauris vitae ligula. Vivamus ut lectus vel sapien ultrices vestibulum. Morbi blandit arcu in risus tempus vestibulum. Pellentesque nec augue sagittis, posuere ante id, maximus lectus. Curabitur iaculis, elit eu congue maximus, orci felis pharetra dolor, sit amet dignissim nunc neque vel neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ante sagittis, congue tortor nec, faucibus nisl. Aliquam lobortis elementum lorem, vel mattis erat. Ut vestibulum augue porta augue pharetra, quis finibus orci ultrices. Nunc nunc tellus, lacinia vitae congue et, ullamcorper in nunc. In at lacus ac velit posuere convallis. Etiam blandit sit amet neque non hendrerit. Etiam quis arcu sit amet arcu facilisis facilisis.
+        </p>
+    </main>
+
+    <footer class="footer">
+        <div class="contenedor">
+            <div class="barra">
+                <a class="logo" href="index.php">
+                   <h1 class="logo__nombre no-margin centrar-texto">Blog<span class="logo__bold">DeCafé</span></h1>
+                </a>
+                <nav class="navegacion">
+                    <a href="nosotros.php" class="navegacion__enlace">Nosotros</a>
+                    <a href="cursos.php" class="navegacion__enlace">Cursos</a>
+                    <a href="contacto.php" class="navegacion__enlace">contacto</a>
+                </nav>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
